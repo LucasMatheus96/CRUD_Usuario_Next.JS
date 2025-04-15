8d862daad02c607e616d5db24d46d360f24acdad
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-04-15 00:08:33 -0300

    Criação do componente Layout

--- fundamentos/src/components/Layout.jsx
+++ fundamentos/src/components/Layout.jsx
@@ -1,8 +1,19 @@
 import Link from "next/link"
+import styles from '../styles/Layout.module.css'
 export default function Layout(props) {
     return(
-        <div>
-            <Link href="/"></Link>
+        <div className={styles.layout}>
+
+            <div className={styles.cabecalho}>
+             <h1>{props.titulo ?? 'Mais um Exemplo'}</h1>
+            <Link href="/">VOLTAR</Link>
+
+            </div>
+           <div className={styles.conteudo}>
+            {props.children}
+
+           </div>
+        
         </div>
 )
 }
--- fundamentos/src/pages/estiloso.jsx
+++ fundamentos/src/pages/estiloso.jsx
@@ -1,10 +1,15 @@
+import Layout from '../components/Layout'
 import styles from '../styles/Estiloso.module.css'
 import Link from 'next/link'
 export default function Estiloso() {
     return (
-            <div className={styles.roxo}>
-                <Link href="/">Voltar</Link>
-                <h1>Estilo usando CSS Módulos</h1>
-            </div>        
+            
+                <Layout titulo="Exemplo de CSS modularizado" >
+                    <div className={styles.roxo}>
+                        <h1>Estilo usando CSS Módulos</h1>
+                    </div>
+               </Layout>
+                
+                 
     )
 }
