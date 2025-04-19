b48b2ee6f8bfbd7364b2292a630ec48bad815276
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-04-19 00:03:09 -0300

    Iiniciando uso de API

---
+++ fundamentos/src/pages/api/clientes/index.js
@@ -0,0 +1,21 @@
+export default function handler(req, res) {
+
+    if(req.method === "GET"){
+        handlerGET(req,res)
+    }else{
+        res.status(405).send('Método não permitido')
+    }
+    res.status(200).json({
+        id:3,
+        nome: 'Ana',
+        idade: 29,
+    })
+}
+
+function handlerGET(req,resp){
+    resp.status(200).json({
+        id:3,
+        nome: 'Ana',
+        idade: 29,
+    })
+}
